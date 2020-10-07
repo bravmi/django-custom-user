3eaeda39535d37433a6ad0143ee4ad9e06b8afb1
Michael T <bravmech@gmail.com>
2020-10-07 13:47:39 +0500

    redirect to admin from root

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+from django.urls import path
 
 urlpatterns = [
     path('admin/logout/', auth_views.LogoutView.as_view(next_page='/admin/')),
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('admin/')),
 ]
