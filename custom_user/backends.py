b4ec5c1b5471de6b796a373d939c3b29929c6316
Michael T <bravmech@gmail.com>
2020-10-07 13:47:51 +0500

    refactor

--- custom_user/backends.py
+++ custom_user/backends.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
 
 UserModel = get_user_model()
 
@@ -18,18 +18,3 @@ class EmailUsernameBackend(ModelBackend):
         if user and user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
-
-    def get_user(self, user_id):
-        try:
-            user = UserModel.objects.get(pk=user_id)
-        except UserModel.DoesNotExist:
-            return None
-        return user if self.user_can_authenticate(user) else None
-
-    def user_can_authenticate(self, user):
-        """
-        Reject users with is_active=False. Custom user models that don't have
-        that attribute are allowed.
-        """
-        is_active = getattr(user, 'is_active', None)
-        return is_active or is_active is None
