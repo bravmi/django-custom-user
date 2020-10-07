b42d1a1b906a2be342ca55006cd1c306ebbcf696
Michael T <bravmech@gmail.com>
2020-10-07 11:07:24 +0500

    refactor tests

--- custom_user/tests.py
+++ custom_user/tests.py
@@ -1,13 +1,15 @@
 import pytest
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import is_password_usable
 from django.db.utils import IntegrityError
 from django.test import TestCase
+
+UserModel = get_user_model()
 
 
 class UsersManagersTests(TestCase):
     def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(email='user@gmail.com', password='foo')
+        user = UserModel.objects.create_user(email='user@gmail.com', password='foo')
         assert user.email == 'user@gmail.com'
         assert user.username is None
 
@@ -16,21 +18,18 @@ class UsersManagersTests(TestCase):
         assert user.is_superuser is False
 
         with pytest.raises(TypeError):
-            User.objects.create_user()
+            UserModel.objects.create_user()
         with pytest.raises(ValueError):
-            User.objects.create_user(email='')
+            UserModel.objects.create_user(email='')
         with pytest.raises(ValueError):
-            User.objects.create_user(email='', password="foo")
+            UserModel.objects.create_user(email='', password="foo")
 
     def test_create_user_with_empty_password(self):
-        User = get_user_model()
-        email = 'user@gmail.com'
-        password = None
-        User.objects.create_user(email=email, password=password)
+        user = UserModel.objects.create_user(email='user@gmail.com', password=None)
+        assert is_password_usable(user.password) is False
 
     def test_create_superuser(self):
-        User = get_user_model()
-        admin = User.objects.create_superuser(email='admin@gmail.com', password='foo')
+        admin = UserModel.objects.create_superuser(email='admin@gmail.com', password='foo')
         assert admin.email == 'admin@gmail.com'
         assert admin.username is None
 
@@ -39,34 +38,35 @@ class UsersManagersTests(TestCase):
         assert admin.is_superuser is True
 
     def test_email_unique(self):
-        User = get_user_model()
-        User.objects.create(email='user@gmail.com', password='foo')
+        UserModel.objects.create_user(email='user@gmail.com', password='foo')
         with pytest.raises(IntegrityError):
-            User.objects.create(email='user@gmail.com', password='foo')
+            UserModel.objects.create_user(email='user@gmail.com', password='foo')
 
     def test_username_unique(self):
-        User = get_user_model()
-        User.objects.create(email='user1@gmail.com', password='foo')
-        User.objects.create(email='user2@gmail.com', password='foo')
+        UserModel.objects.create_user(email='user1@gmail.com', username='user', password='foo')
+        with pytest.raises(IntegrityError):
+            UserModel.objects.create_user(email='user2@gmail.com', username='user', password='foo')
+
+    def test_username_unique_none(self):
+        UserModel.objects.create_user(email='user1@gmail.com', password='foo')
+        UserModel.objects.create_user(email='user2@gmail.com', password='foo')
+        assert UserModel.objects.count() == 2
 
     def test_login_with_email(self):
-        User = get_user_model()
         email = 'user@gmail.com'
         password = 'foo'
-        User.objects.create_user(email=email, password=password)
+        UserModel.objects.create_user(email=email, password=password)
         assert self.client.login(username=email, password=password) is True
 
     def test_login_with_empty_username(self):
-        User = get_user_model()
         email = 'user@gmail.com'
         password = 'foo'
-        User.objects.create_user(email=email, password=password)
+        UserModel.objects.create_user(email=email, password=password)
         assert self.client.login(username=None, password=password) is False
 
     def test_login_with_username(self):
-        User = get_user_model()
         email = 'user@gmail.com'
         username = 'user'
         password = 'foo'
-        User.objects.create_user(email=email, username=username, password=password)
+        UserModel.objects.create_user(email=email, username=username, password=password)
         assert self.client.login(username=username, password=password) is True
