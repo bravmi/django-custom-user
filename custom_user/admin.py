0a03c1be35de5b6cb3226b2f2fa85f26cce1f1c1
Michael T <bravmech@gmail.com>
2020-10-15 18:23:16 +0500

    remove is_staff from admin

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -109,11 +109,11 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ['email', 'username', 'is_active', 'is_staff', 'is_superuser']
+    list_display = ['email', 'username', 'is_active', 'is_superuser']
     list_filter = ['is_superuser']
     fieldsets = [
         (None, {'fields': ['email', 'username', 'password']}),
-        ('Permissions', {'fields': ['is_staff', 'is_superuser']}),
+        ('Permissions', {'fields': ['is_superuser']}),
     ]
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
